c40b52578f5d3199b61c06149ecbaf7aed3c0e52
NeilDogii <aryaworrior41@gmail.com>
2024-04-22 03:01:11 +0530

    added some functions

--- app.js
+++ app.js
@@ -2,10 +2,15 @@ var element = document.getElementsByClassName("btn");
 for (var i = 0 ; i < 16; i++) {
     element[i].addEventListener('click' , function(e) {
         console.log(e.target.firstChild.data)
-        //document.getElementsByClassName("display")[0].innerHTML = ;
+        var str = document.getElementsByClassName("display")[0].value
+        if(e.target.firstChild.data == "C"){
+            document.getElementsByClassName("display")[0].value = str.substring(0, str.length - 1);
+        }else if(e.target.firstChild.data == "="){
+            var str = document.getElementsByClassName("display")[0].value
+            document.getElementsByClassName("display")[0].value = eval(str);
+        }else{
+            document.getElementsByClassName("display")[0].value += e.target.firstChild.data;
+        }
+        
     })
  }
-// element.addEventListener("click", function() {
-//     console.log("Button clicked")
-//   document.getElementById("demo").innerHTML = "Hello World";
-// });
