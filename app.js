3aec19ba18e567576424a57c27ca5ba9a5b74838
NeilDogii <aryaworrior41@gmail.com>
2024-04-22 20:28:06 +0530

    added some checks

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
 var element = document.getElementsByClassName("btn");
+var signs = ['+', '-', '*', '/']
 for (var i = 0 ; i < 16; i++) {
     element[i].addEventListener('click' , function(e) {
-        console.log(e.target.firstChild.data)
+        console.log(!isNaN(e.target.firstChild.data))
+        console.log(!signs.includes(e.target.firstChild.data))
+        if(isNaN(e.target.firstChild.data) || !signs.includes(e.target.firstChild.data)){
+        console.log("wont run")
+        }
         var str = document.getElementsByClassName("display")[0].value
+        var expression = ""
         if(e.target.firstChild.data == "C"){
             document.getElementsByClassName("display")[0].value = str.substring(0, str.length - 1);
         }else if(e.target.firstChild.data == "="){
             var str = document.getElementsByClassName("display")[0].value
-            document.getElementsByClassName("display")[0].value = eval(str);
+            document.getElementsByClassName("display")[0].value = eval(str)
         }else{
             document.getElementsByClassName("display")[0].value += e.target.firstChild.data;
         }
