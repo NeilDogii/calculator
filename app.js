e1d49a822e6912a5868f79f1c5c6f6f0785c2382
NeilDogii <aryaworrior41@gmail.com>
2024-04-23 19:08:15 +0530

    calculator final commit (hopefully)

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var element = document.getElementsByClassName("btn");
 var signs = ['+', '-', '*', '/']
+var expression = ""
 for (var i = 0 ; i < 16; i++) {
     element[i].addEventListener('click' , function(e) {
-        console.log(!isNaN(e.target.firstChild.data))
-        console.log(!signs.includes(e.target.firstChild.data))
-        if(isNaN(e.target.firstChild.data) || !signs.includes(e.target.firstChild.data)){
-        console.log("wont run")
+        //console.log(!isNaN(e.target.firstChild.data), signs.includes(e.target.firstChild.data))
+        if(isNaN(e.target.firstChild.data) && !signs.includes(e.target.firstChild.data) && e.target.firstChild.data != "C" && e.target.firstChild.data != "="){
+        return console.error("Illegal format, please do no change the input. If the problem persists, please reload the page.")
         }
         var str = document.getElementsByClassName("display")[0].value
-        var expression = ""
         if(e.target.firstChild.data == "C"){
             document.getElementsByClassName("display")[0].value = str.substring(0, str.length - 1);
+            expression = expression.substring(0, expression.length - 1)
         }else if(e.target.firstChild.data == "="){
-            var str = document.getElementsByClassName("display")[0].value
-            document.getElementsByClassName("display")[0].value = eval(str)
+            document.getElementsByClassName("display")[0].value = eval(expression)
+            expression = eval(expression)
         }else{
             document.getElementsByClassName("display")[0].value += e.target.firstChild.data;
+            expression += e.target.firstChild.data
         }
         
     })
